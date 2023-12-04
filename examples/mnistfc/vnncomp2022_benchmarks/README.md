a1b4c8615a4752eb633407a78a77a9f3034f0f66
niemaaand <jan.schwab@gmx.net>
2023-12-04 09:50:58 +0100

    Add newer benchmark vnnlib files.

---
+++ examples/mnistfc/vnncomp2022_benchmarks/README.md
@@ -0,0 +1,2 @@
+from vnncomp 2022 mnistfc benchmark:
+https://github.com/ChristopherBrix/vnncomp2022_benchmarks/tree/main/benchmarks/mnist_fc/vnnlib
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
 
     sys.argv.append(
         "examples/mnistfc/0.9217812418937683_f1Score__8_Epoch__2023_11_21_17_37_24_options.json_Options.onnx")
-    sys.argv.append("examples/mnistfc/prop_0_0.03.vnnlib")
+    sys.argv.append("examples/mnistfc/vnncomp2022_benchmarks/prop_0_0.03.vnnlib")
+    #sys.argv.append("C:\\Code\\KIT\\vnncomp2022_benchmarks\\benchmarks\\mnist_fc\\vnnlib\\prop_0_0.03.vnnlib")
 
     nnenum_file.main()
